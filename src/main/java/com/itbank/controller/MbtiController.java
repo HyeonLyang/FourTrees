42e15ccc8a0947160d97c0f517ea337842d2053e
cnusk <kingammonite@naver.com>
2023-12-11 13:37:27 +0900

    safe zone

--- src/main/java/com/itbank/controller/MbtiController.java
+++ src/main/java/com/itbank/controller/MbtiController.java
@@ -22,58 +22,17 @@ public class MbtiController {
 	@GetMapping("/mbti_research")
 	public void mbti_research() {}
 	
-	/*@GetMapping("/t{mbti}")
+	@GetMapping("/mbti_result")
+	public void mbti_result() {}
+	
+	@GetMapping("/test/{mbti}")
 	public ModelAndView Test(@PathVariable String mbti)  {
-		ModelAndView mav = new ModelAndView()
-	} */
-	
-	@GetMapping("/istj")
-	public void istj() {}
-	
-	@GetMapping("/isfj")
-	public void isfj() {}
-	
-	@GetMapping("/infj")
-	public void infj() {}
-	
-	@GetMapping("/intj")
-	public void intj() {}
-	
-	@GetMapping("/istp")
-	public void istp() {}
-	
-	@GetMapping("/isfp")
-	public void isfp() {}
-	
-	@GetMapping("/infp")
-	public void infp() {}
-	
-	@GetMapping("/intp")
-	public void intp() {}
-	
-	@GetMapping("/estp")
-	public void estp() {}
-	
-	@GetMapping("/esfp")
-	public void esfp() {}
-	
-	@GetMapping("/enfp")
-	public void enfp() {}
-	
-	@GetMapping("/entp")
-	public void entp() {}
-	
-	@GetMapping("/estj")
-	public void estj() {}
-	
-	@GetMapping("/esfj")
-	public void esfj() {}
-	
-	@GetMapping("/enfj")
-	public void enfj() {}
-	
-	@GetMapping("/entj")
-	public void entj() {}
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("mbti/mbti_result");
+		mav.addObject("result", mbti);
+		
+		return mav;
+	}
 	
 	/*
 	 * @PostMapping("/mbti_research") public ModelAndView check(HttpSession session)
