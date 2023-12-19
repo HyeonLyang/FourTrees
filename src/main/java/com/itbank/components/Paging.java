f4a303272401e88e85c1cda757ce492b6c63a78e
JunwooHa <ahffk969j@naver.com>
2023-12-19 13:39:46 +0900

    Squashed commit of the following:
    commit f4669d2f0860b205d0281be91a20fcf0a2472f01
    Merge: de46619 0ead050
    Author: JunwooHa <44633312+JunwooHa@users.noreply.github.com>
    Date:   Tue Dec 19 13:38:39 2023 +0900
    
        Merge pull request #37 from HyeonLyang/창현
    
        창현
    
    commit 0ead050b0c59bad6c0d904b964169d50e4aeefc0
    Author: ChangHyeon ji <ji626501@gmail.com>
    Date:   Tue Dec 19 13:36:24 2023 +0900
    
        1219오류 수정
    
    commit a5b60c452eb44aafc358d59ffb48550d67b64f4b
    Merge: ffee84d 2ca3a19
    Author: ChangHyeon ji <ji626501@gmail.com>
    Date:   Tue Dec 19 13:29:33 2023 +0900
    
        Merge commit '2ca3a199fe64a60c4f01acd362fda38331a557c6' into 창현
    
    commit 2ca3a199fe64a60c4f01acd362fda38331a557c6
    Author: ChangHyeon ji <ji626501@gmail.com>
    Date:   Tue Dec 19 13:19:06 2023 +0900
    
        1219 master 수정
    
    commit de466191475348f99b17e62df0e9e923a592d714
    Merge: 772f594 11e50b9
    Author: HyeonLyang <140620467+HyeonLyang@users.noreply.github.com>
    Date:   Tue Dec 19 13:15:09 2023 +0900
    
        Merge pull request #36 from HyeonLyang/하준우
    
        하준우
    
    commit ffee84d762b0b21db0170d2a8b6b72a0bc720c93
    Author: ChangHyeon ji <ji626501@gmail.com>
    Date:   Tue Dec 19 11:45:01 2023 +0900
    
        Merge commit 'd6a4ad59b2351d688b7ad246c97c964605cdaa57'

--- src/main/java/com/itbank/components/Paging.java
+++ src/main/java/com/itbank/components/Paging.java
@@ -13,9 +13,8 @@ public class Paging {
 	private String address, category;
 
 	public Paging() {
-    
-  }
-
+		
+	}
 
 	public Paging(int reqPage, int totalRestaurant) {
 		this.reqPage = reqPage;
