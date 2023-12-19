eefa54e1f94c0fbe410506c402221b2a755e5ca7
cnusk <kingammonite@naver.com>
2023-12-19 13:37:24 +0900

    123

--- src/main/java/com/itbank/components/Paging.java
+++ src/main/java/com/itbank/components/Paging.java
@@ -15,25 +15,6 @@ public class Paging {
 	public Paging() {
 		
   }
-	
-	public String getAddress() {
-		return address;
-	}
-
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-
-	public String getCategory() {
-		return category;
-	}
-
-
-	public void setCategory(String category) {
-		this.category = category;
-	}
 
 	public Paging(int reqPage, int totalRestaurant) {
 		this.reqPage = reqPage;
