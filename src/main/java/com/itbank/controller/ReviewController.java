ec22f18e525f12e14c440fa40db28b771b74de1e
JunwooHa <ahffk969j@naver.com>
2023-12-21 09:46:50 +0900

    1221 식탕 페이지에 리뷰 쓰기 추가 및 이미지 팝업창 수정

--- src/main/java/com/itbank/controller/ReviewController.java
+++ src/main/java/com/itbank/controller/ReviewController.java
@@ -66,7 +66,7 @@ public class ReviewController {
 	@PostMapping("/review_write/{name}/{res_idx}")
 	public ModelAndView write(ReviewVO input,String score,
 							  @PathVariable int res_idx, @PathVariable String name) throws Exception {
-		ModelAndView mav = new ModelAndView("redirect:/review/review/{name}/{res_idx}") {};
+		ModelAndView mav = new ModelAndView("redirect:/restaurant/res_detail/{res_idx}") {};
 		
 		double num = Double.parseDouble(score);
 		
@@ -81,7 +81,7 @@ public class ReviewController {
 	@GetMapping("/delete/{idx}/{name}/{res_idx}")
 	public ModelAndView delete(@PathVariable int idx,@PathVariable String name,
 							   @PathVariable int res_idx,ReviewVO input) {
-		ModelAndView mav = new ModelAndView("redirect:/review/review/{name}/{res_idx}");
+		ModelAndView mav = new ModelAndView("redirect:/restaurant/res_detail/{res_idx}");
 		
 		rvs.delete(input);
 		
