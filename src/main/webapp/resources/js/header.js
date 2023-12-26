45635257451f7fd9cc21941188146a863e023498
kwonhyunjae <guswo1118@naver.com>
2023-12-26 13:21:12 +0900

    Merge commit '3f25b1bc93a4bc01ec27d910dc704784dfd41035'

--- src/main/webapp/resources/js/header.js
+++ src/main/webapp/resources/js/header.js
@@ -1,3 +1,12 @@
+/* 헤더2 번역기능 토글 */
+var languageContainer = document.getElementsByClassName("language-container")[0];
+var isOpen = false;
+
+function toggle2() {
+    isOpen = !isOpen;
+    languageContainer.style.width = isOpen ? "300px" : "55px";
+}
+
 /* 헤더 메뉴아이콘 토글 */
 var menu_container = document.getElementsByClassName("menu_container")[0];
 var sub_container = document.getElementsByClassName("sub_container")[0];
