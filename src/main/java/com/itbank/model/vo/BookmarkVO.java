ebe94e2a86680082e0db96d521a7bbd51eadd62d
kwonhyunjae <guswo1118@naver.com>
2023-12-31 11:18:03 +0900

    123

--- src/main/java/com/itbank/model/vo/BookmarkVO.java
+++ src/main/java/com/itbank/model/vo/BookmarkVO.java
@@ -13,24 +13,8 @@ public class BookmarkVO {
 	private int idx, acc_idx, res_idx;
 	private String res_name,res_address;
 	private Date mark_date;
-	
-	private String res_name;  // 가게 이름을 저장할 필드 추가
-    private String res_address;  // 가게 주소를 저장할 필드 추가
-	
     
 	
-	public String getRes_name() {
-		return res_name;
-	}
-	public void setRes_name(String res_name) {
-		this.res_name = res_name;
-	}
-	public String getRes_address() {
-		return res_address;
-	}
-	public void setRes_address(String res_address) {
-		this.res_address = res_address;
-	}
 	public int getIdx() {
 		return idx;
 	}
